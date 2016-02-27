5297f6a6df8eb514260a7f48e49f9fa8da11616c
bcohn12 <bcohn12@gmail.com>
2016-02-26 16:00:55 -0800

    spelling & formatting updates

--- BFA_hang.py
+++ BFA_hang.py
@@ -20,14 +20,14 @@ args = parser.parse_args()
 if args.help:
     print "This program takes in a file list containing full paths of certain type of files as argument and output an unified average byte frequency of all the file in file list"
     print "One sample row for the file list is: 160/201/178/155/C5402A255D63ED25FD81A0D9093C70B571D2FE2D3D0875BBA37AE674FD14D1EB"
-    print "An example call to the scrtip: python BFA_hang.py --file_list_path ./test_list --output test_out.signature which generate the byte freq signature test_out.signature for file listed in test_list"
+    print "An example call to the script: python BFA_hang.py --file_list_path ./test_list --output test_out.signature which generate the byte freq signature test_out.signature for file listed in test_list"
     sys.exit()
 
-#Function:Count the avarge occurance of byte 0~255 in all the files contained in the inputted list 
-#	  Average_occunrace=total_occurance/file_count
+#Function:Count the average occurance of byte 0~255 in all the files contained in the inputted list 
+#	  Average_occurance=total_occurance/file_count
 #Input:Full path of a list containing all data files of certain MIME type
 #Output:256 element python list containing the average occurance count of byte 0~255 in those files (occurance of byte x can be retrived as list[x])
-def count_avg_byte_occurance(file_list_path):
+def count_average_byte_occurance(file_list_path):
 	file_list_fd=open(file_list_path,'r')
 	file_count=0
 	abolist=[0]*256	
@@ -51,7 +51,7 @@ def count_avg_byte_occurance(file_list_path):
 	return abolist
 
 #Function: convert a list containing averge byte occurance to a list containing byte freq
-def abo2bf(abolist):
+def occurrence_to_byte_frequency(abolist):
 	max_bl=max(abolist)
 	abolist[:]=[x/max_bl*1.0 for x in abolist]
 	return abolist
@@ -59,8 +59,8 @@ def abo2bf(abolist):
 	 
 flp=args.file_list_path
 out_fd=open(str(args.output_file_name),'w')
-abolist=count_avg_byte_occurance(flp)#abolist is a list containing average bype occurance
-bflist=abo2bf(abolist) #bflist is a list containing byte freq
+abolist=count_average_byte_occurance(flp)#abolist is a list containing average bype occurance
+bflist=occurrence_to_byte_frequency(abolist) #bflist is a list containing byte freq
 out_fd.write("\n".join(map(str,bflist)))
 out_fd.close()
 if int(args.debug)==1:
