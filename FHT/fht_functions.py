9dd855cc65342f3615516f6a51ee51b645159ce0
bcohn12 <bcohn12@gmail.com>
2016-03-03 20:01:43 -0800

    working file list reader for Hang. See #8

--- FHT/fht_functions.py
+++ FHT/fht_functions.py
@@ -1,13 +1,12 @@
 #TODO implement the matrix generation for the footer work.
 #TODO run on the whole dataset.
 
-
+import sys, argparse, json
 import os
-import ipdb
+
 import numpy as np
 import matplotlib.pyplot as plt
 # http://stackoverflow.com/questions/1035340/reading-binary-file-in-python-and-looping-over-each-byte
-
 #Function:Count the avarge occurance of byt
 		
 def test_file_list():
@@ -29,6 +28,9 @@ def test_file_list():
 				"test_img_files/nine_bytes.jpg",
 				"test_img_files/eighteen_bytes.jpg"]
 				)
+def read_file_list(file_list_path="default_file_list"):
+	list = [str(x).rstrip('\n') for x in open(file_list_path,'r')]
+	return(list)
 
 def gen_expected_distribution_for_one_byte_b():
 	neg1_list = np.ones(256)*-1
@@ -40,9 +42,13 @@ def gen_expected_matrix_for_two_short_files():
 	mat = np.vstack([np.zeros(256), neghalflist,neghalflist,neghalflist])
 	return(mat)
 
-def bytes_from_file(filename, chunksize=8192):
+def bytes_from_file(filename, chunksize, num_bytes_to_collect,is_trailer):
 	with open(filename, "rb") as f:
 		while True:
+			if is_trailer:
+				#set the position to be read backwards
+				index_near_end = -1 * num_bytes_to_collect
+				f.seek(index_near_end,2)
 			chunk = f.read(chunksize)
 			if chunk:
 				for b in chunk:
@@ -50,10 +56,10 @@ def bytes_from_file(filename, chunksize=8192):
 			else:
 				break
 
-def bytes(filename, num_bytes_to_collect):
+def bytes(filename, num_bytes_to_collect, is_trailer):
 	L = []
 	bytes_recorded_so_far=0
-	for b in bytes_from_file(filename):
+	for b in bytes_from_file(filename, 8192,num_bytes_to_collect, is_trailer):
 		L += [b]
 		bytes_recorded_so_far += 1
 		if bytes_recorded_so_far == num_bytes_to_collect:
@@ -62,22 +68,23 @@ def bytes(filename, num_bytes_to_collect):
 
 #returns just an integer of the num bytes in a file
 def get_length_of_file_in_bytes(filename):
-	return(sum([1 for b in bytes_from_file(filename)]))
+	return(os.path.getsize(filename))
 
-def bytes_backwards(filename, num_bytes_to_collect):
-	testdata = range(num_bytes_to_collect)
-	return(testdata)
+# def bytes_backwards(filename, num_bytes_to_collect):
+# 	with open(filename, "rb") as f:
+# 		f.seek(num_bytes_to_collect, 2)
+# 	return(testdata)
 
 
 def extract_first_n_elements(n, L):
 	return(L[0:n])
 
 def extract_n_header_bytes_from_file(filename, number_of_bytes):
-	header_bytes = bytes(filename, number_of_bytes)
+	header_bytes = bytes(filename, number_of_bytes, 0)
 	return(header_bytes)
 
 def extract_n_footer_bytes_from_file(filename, number_of_bytes):
-	footer_bytes = bytes_backwards(filename, number_of_bytes) #Reverse the bytes so we can reuse the 
+	footer_bytes = bytes(filename, number_of_bytes, 1) #Reverse the bytes so we can reuse the 
 	return(footer_bytes)
 
 def zero_matrix_for_FHT(first_n_bytes):
@@ -95,16 +102,16 @@ def build_FHT_header_matrix(filename, first_n_bytes):
 	header_matrix = plug_in_negative_numbers_if_file_is_shorter_than_header_len(filename, first_n_bytes, header_matrix)
 	return(header_matrix)
 
-def build_FHT_footer_matrix(filename, first_n_bytes):
+def build_FHT_footer_matrix(filename, last_n_bytes):
 	#Instantiated the matrix of zeros
-	footer_matrix =  zero_matrix_for_FHT(first_n_bytes)
+	footer_matrix =  zero_matrix_for_FHT(last_n_bytes)
 	#Grab the file header bytes
-	file_footer_bytes = extract_n_footer_bytes_from_file(filename,first_n_bytes)
+	file_footer_bytes = extract_n_footer_bytes_from_file(filename,last_n_bytes)
 	counter = 0
 	for byte_value in file_footer_bytes:
 		footer_matrix[counter,byte_value] = 1
 		counter += 1
-	footer_matrix = plug_in_negative_numbers_if_file_is_shorter_than_header_len(filename, first_n_bytes, footer_matrix)
+	footer_matrix = plug_in_negative_numbers_if_file_is_shorter_than_header_len(filename, last_n_bytes, footer_matrix)
 	return(footer_matrix)	
 
 def plug_in_negative_numbers_if_file_is_shorter_than_header_len(filename,first_n_bytes,header_matrix):
@@ -188,7 +195,7 @@ def run_header_and_footer(type_name, list_of_filenames, bytes_to_check):
 	# Generate output files into those filenames
 	# One for header and one for trailer
 	save_csv_for_each_byte_len(list_of_filenames, filename_out = header_FHT_filename_out, bytes_to_check=bytes_to_check, header_or_footer="header")
-	save_csv_for_each_byte_len(list_of_filenames, filename_out = footer_FHT_filename_out, bytes_to_check=bytes_to_check, header_or_footer="footer")
+	# save_csv_for_each_byte_len(list_of_filenames, filename_out = footer_FHT_filename_out, bytes_to_check=bytes_to_check, header_or_footer="footer")
 
 
 def save_csv_for_each_byte_len(list_of_filenames, filename_out, bytes_to_check, header_or_footer):
@@ -204,6 +211,7 @@ def read_first_n_bytes(filename, num_bytes):
 	# while 1:
 	byte = file.read(num_bytes)
 	return byte
+
 
 if __name__ == '__main__':
 	# unittest.main()
